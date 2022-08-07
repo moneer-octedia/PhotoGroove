b4035b3866b235b506de7197dcde90e5e3ba3211
Moneer R. Alhashim <61453768+moneer-muntazah@users.noreply.github.com>
2022-08-07 12:03:38 +0300

    custom type

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -18,8 +18,12 @@ type alias Model =
     , chosenSize : ThumbnailSize
     }
 
-type alias Msg =
-    { description : String, data : String }
+-- type alias Msg =
+--     { description : String, data : String }
+type Msg 
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
 
 urlPrefix =
     "http://elm-in-action.com/"
@@ -29,7 +33,7 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button
-            [ onClick { description = "ClickedSurpriseMe", data = ""} ]
+            [ onClick ClickedSurpriseMe ]
             [ text "Surprise Me!" ]
         , h3 [] [ text "Thumbnail Size:" ]
         , div [ id "choose-size" ]
@@ -48,14 +52,14 @@ viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
+        , onClick (ClickedPhoto thumb.url) 
         ]
         []
 
 viewSizeChooser : ThumbnailSize -> Html Msg
 viewSizeChooser size =
     label []
-        [ input [ type_ "radio", name "size" ] []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
         , text (sizeToString size)
         ]
 
@@ -90,14 +94,13 @@ getPhotoUrl index =
 
 update : Msg -> Model -> Model
 update msg model = 
-    case msg.description of 
-        "ClickedPhoto" ->
-            { model | selectedUrl = msg.data }
-        "ClickedSurpriseMe" ->
+    case msg of 
+        ClickedPhoto url ->
+            { model | selectedUrl = url }
+        ClickedSize size ->
+            { model | chosenSize = size}
+        ClickedSurpriseMe ->
             { model | selectedUrl = "2.jpeg" }
-        _->
-             model
-
 main =
     Browser.sandbox
         { init = initialModel
